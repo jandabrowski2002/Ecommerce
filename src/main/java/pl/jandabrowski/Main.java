9386b8b2ef5b85ae9938413696f21e18713af17b
en-013-25 <en-013-25@gumiforest.uek.krakow.pl>
2023-03-06 12:50:21 +0100

    played with functional api

--- src/main/java/pl/jandabrowski/Main.java
+++ src/main/java/pl/jandabrowski/Main.java
@@ -1,7 +1,36 @@
 package pl.jandabrowski;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<String> names = Arrays.asList("Jakub", "Michal", "Agnieszka", "Ola", "Kasia");
+        names.add("Jasiek");
+
+        Greeter greeter = new Greeter();
+        greeter.greet("Jakub"); // -> Hello Jakub
+
+        // Greet only females
+        List<String> females = new ArrayList<String>();
+        for (String name : names){
+            if (name.endsWith("a")){
+                females.add(name);
+
+            }
+        }
+        for (String ladyName : females){
+            greeter.greet(ladyName);
+        }
+
+        //names.stream()
+                //.filter(name -> name.endsWith("a"))
+                //.forEach(ladyName -> greeter::greet);
+
+
+
+
+
     }
 }
